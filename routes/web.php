ef2202b0fc81183679dc4ebe11ab62aea42a5023
Naura <naurazahra448@gmail.com>
2026-01-05 15:35:09 +0700

    day 14-20

--- routes/web.php
+++ routes/web.php
@@ -103,9 +103,7 @@ Route::middleware(['auth', 'admin'])
     ->group(function () {
 
         // ✅ DASHBOARD TANPA CONTROLLER
-        Route::get('/', function () {
-            return view('admin.dashboard');
-        })->name('dashboard');
+         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
         // Produk
         Route::resource('products', AdminProductController::class);
@@ -200,3 +198,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/orders/{order}/pending', [PaymentController::class, 'pending'])
         ->name('orders.pending');
 });
+
+// routes/web.php
+
+use App\Http\Controllers\MidtransNotificationController;
+
+// ============================================================
+// MIDTRANS WEBHOOK
+// Route ini HARUS public (tanpa auth middleware)
+// Karena diakses oleh SERVER Midtrans, bukan browser user
+// ============================================================
+Route::post('midtrans/notification', [MidtransNotificationController::class, 'handle'])
+    ->name('midtrans.notification');
+
+    // Batasi 5 request per menit
+Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:5,1');
